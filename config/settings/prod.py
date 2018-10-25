d12365a94617d2330bfda0f81364c2686a78e4ad
DESKTOP-B43K2VI\d0nald <bestwork888@outlook.com>
2018-10-25 09:03:41 -0700

    dfasdfa

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,25 +2,25 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
-#ALLOWED_HOSTS = [
-#    '*',  # heroku url
-#]
+ALLOWED_HOSTS = [
+    '*',  # heroku url
+]
 
-#DATABASES = {'default': dj_database_url.config()}
-#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES = {'default': dj_database_url.config()}
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
-#MIDDLEWARE += [
-#    'whitenoise.middleware.WhiteNoiseMiddleware',
-#]
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 
-#STATICFILES_FINDERS = (
-#    'django.contrib.staticfiles.finders.FileSystemFinder',
-#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#   'compressor.finders.CompressorFinder',
-#)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+   'compressor.finders.CompressorFinder',
+)
 
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#DEFAULT_FILE_STORAGE = 'whitenoise.storage.'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'whitenoise.storage.'
